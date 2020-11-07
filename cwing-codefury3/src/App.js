c6d9cd6f0eb2afdcbf8fd523980bcdd05ca254fe
Vishal M <vishalm2390@gmail.com>
2020-11-07 13:34:03 +0530

    SignIn, react route, landing page

--- cwing-codefury3/src/App.js
+++ cwing-codefury3/src/App.js
@@ -1,11 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Route, BrowserRouter } from 'react-router-dom';
+import LandingPage from './pages/LandingPage.js';
+import LabourerSignIn from './pages/LabourerSignIn';
+import EmployerSignIn from './pages/EmployerSignIn'
 
 function App() {
   return (
-    <div className="App">
-      <H1>C WING</H1>
-    </div>
+    <React.Fragment>
+      <BrowserRouter>
+        <div className="App">
+          <Route exact path='/'>
+            <LandingPage />
+          </Route>
+          <Route path='/empsignin'>
+            <EmployerSignIn />
+          </Route>
+          <Route path='/labsignin'>
+            <LabourerSignIn />
+          </Route>
+        </div>
+      </BrowserRouter>
+    </React.Fragment>
+
   );
 }
 
