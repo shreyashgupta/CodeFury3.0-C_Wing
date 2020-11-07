46e95d7702974db406868701dd9e561ce80476e5
Vishal M <vishalm2390@gmail.com>
2020-11-07 15:20:00 +0530

    Shreyash and Vishal Merged

--- cwing-codefury3/src/pages/LabourerSignIn.js
+++ cwing-codefury3/src/pages/LabourerSignIn.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'tachyons';
+import InstagramIcon from '@material-ui/icons/Instagram';
 
 class Signin extends React.Component {
   constructor(props) {
@@ -42,6 +43,7 @@ class Signin extends React.Component {
       <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
         <main className="pa4 black-80">
           <div className="measure">
+            <InstagramIcon/>
             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
               <legend className="f1 fw6 ph0 mh0">Labourer Sign In</legend>
               <div className="mt3">
