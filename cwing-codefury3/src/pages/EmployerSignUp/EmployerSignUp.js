a17c342892471dbb307cb79dca75644643dcd254
shreyashgupta <shreyashgupta88@gmail.com>
2020-11-07 18:12:00 +0530

    css

--- cwing-codefury3/src/pages/EmployerSignUp/EmployerSignUp.js
+++ cwing-codefury3/src/pages/EmployerSignUp/EmployerSignUp.js
@@ -7,7 +7,7 @@ import FormControl from '@material-ui/core/FormControl';
 import ListItemText from '@material-ui/core/ListItemText';
 import Select from '@material-ui/core/Select';
 import Checkbox from '@material-ui/core/Checkbox';
-
+import './style.css'
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
 const MenuProps = {
@@ -102,15 +102,15 @@ class EmployerSignUp extends React.Component {
 
     render() {
         return (
-            <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+            <article className="br3 ba b--black-10 mv4 tc w-00 w-50-m w-25-l mw6 shadow-5 center main">
                 <main className="pa4 black-80">
                     <div className="measure">
                         <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
                             <legend className="f1 fw6 ph0 mh0">Employer SignUp</legend>
-                            <div className="mt3">
+                            <div className="mt3 center">
                                 <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
                                 <input
-                                    className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                                    className="pa2 input-reset ba bg-transparent hover-bg-black hover-white "
                                     type="text"
                                     name="name"
                                     id="name"
@@ -120,7 +120,7 @@ class EmployerSignUp extends React.Component {
                             <div className="mt3">
                                 <label className="db fw6 lh-copy f6" htmlFor="date">Date Of Birth</label>
                                 <input
-                                    className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                                    className="pa2 input-reset ba bg-transparent hover-bg-black hover-white "
                                     type="date"
                                     name="date"
                                     id="data"
@@ -130,7 +130,7 @@ class EmployerSignUp extends React.Component {
                             <div className="mt3">
                                 <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
                                 <input
-                                    className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                                    className="pa2 input-reset ba bg-transparent hover-bg-black hover-white "
                                     type="email"
                                     name="email-address"
                                     id="email-address"
@@ -141,7 +141,7 @@ class EmployerSignUp extends React.Component {
                             <div className="mv3">
                                 <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
                                 <input
-                                    className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                                    className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white "
                                     type="password"
                                     name="password"
                                     id="password"
